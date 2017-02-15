f5945b13376409832a3cefc05f680775205b1ea5
senemgenberova <senemgenberova@gmail.com>
2017-02-15 21:08:09 +0400

    Update eight_to_six.java

--- eight_to_six.java
+++ eight_to_six.java
@@ -1,10 +1,23 @@
 public class eight_to_six{
 	public static void main(String[] args){
 		int n = Integer.parseInt(args[0]);
-		int n1 = 0, i=0;
-		n = n/10;
-		n = n%1000000;
-		System.out.println(n);
+		int n1 = 0, k = 0;
+		int temp = n;
+		
+		while(temp!=0){
+			n1 = temp%10;
+			temp = (temp-n1)/10;
+			k++;
+		}
+		
+		if(k==8){
+			n = n/10;
+			n = n%1000000;
+			System.out.println(n);
+		}
+		else{
+			System.out.println("Wrong input");
+		}
 		
 	}
 }
