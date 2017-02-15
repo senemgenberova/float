76d2bde865f516d01eca74b42c827fb62054dd5a
senemgenberova <senemgenberova@gmail.com>
2017-02-15 21:13:51 +0400

    Update sumOfSix.java

--- sumOfSix.java
+++ sumOfSix.java
@@ -1,13 +1,23 @@
 public class sumOfSix{
 	public static void main(String[] args){
 		int n = Integer.parseInt(args[0]);
-		int sum = 0;
-		n = n/1000;
+		int temp = n;
+		int sum = 0 , k=0, n1=0;
 		
-		while(n!=0){
-			sum = sum + n%10;
-			n = n/10;
+		while(temp!=0){
+			n1 = temp%10;
+			temp = (temp-n1)/10;
+			k++;
 		}
-		System.out.println(sum);
+		
+		if(k==8){
+			n = n/1000;
+			while(n!=0){
+				sum = sum + n%10;
+				n = n/10;
+			}
+			System.out.println(sum);
+		}
+		
 	}
 }
